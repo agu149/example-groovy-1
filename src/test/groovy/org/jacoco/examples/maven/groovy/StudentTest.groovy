4268acbd0ce33c06bc3ae771579111e0c58f8397
Ankit Gupta <agupta177@sapient.com>
2018-08-03 02:25:01 +0100

    new

--- src/test/groovy/org/jacoco/examples/maven/groovy/StudentTest.groovy
+++ src/test/groovy/org/jacoco/examples/maven/groovy/StudentTest.groovy
@@ -3,7 +3,7 @@ package org.jacoco.examples.maven.groovy
 class StudentTest extends GroovyTestCase {
    void testDisplay() {
       def stud = new Student(name : 'Joe', ID : '1')
-      def expected = 'Joe1'
+      def expected = 'Joe49'
       assertToString(stud.Display(), expected)
    }
 }
